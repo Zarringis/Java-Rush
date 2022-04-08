70a04a737160585cec9606239753b8f6d0d703e1
Karina Reed <zarringis@gmail.com>
2022-04-08 11:46:42 +0300

    Использование цикла while

---
+++ Cycle.java
@@ -0,0 +1,13 @@
+public class Cycle {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int x = 0;
+        while (x < 10){
+        System.out.println(quote);
+        x++;}
+
+
+
+
+    }
+}
