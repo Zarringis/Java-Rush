96cc8d0d15d1c0e56f7f21999b219c1c88f7ba0c
Karina Reed <zarringis@gmail.com>
2022-04-13 15:32:00 +0300

    Незаполненый прямоугольник с помощью Цикла в цикле

---
+++ CycleRectangle.java
@@ -0,0 +1,32 @@
+public class CycleRectangle {
+
+    /*
+4. 4 Незаполненный прямоугольник
+*/
+    public static void main(String[] args) {
+        int a = 1; //нужен один цикл
+        while (a > 0) {
+            int d = 20; // 1 строчка сплошная
+            while (d > 0) {
+                System.out.print('Б');
+                d--;
+            }
+
+            System.out.println();
+
+            int b = 8; // 8 строчек с пробелом по середине
+            while (b > 0) {
+                System.out.println("Б                  Б");
+                b--;
+            }
+
+            int c = 20; //повторение 1-го цикла внутри цикла
+            while (c > 0) {
+                System.out.print('Б');
+                c--;
+            }
+            a--;
+            System.out.println();
+        }
+    }
+}
