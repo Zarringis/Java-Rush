834a316d07f3e2f7e93e2a74d3ed9df63488570d
Karina Reed <zarringis@gmail.com>
2022-04-13 15:23:27 +0300

    Заполненый прямоугольник с помощью Цикла в цикле

---
+++ CycleFilledRectangle.java
@@ -0,0 +1,20 @@
+public class CycleFilledRectangle {
+
+    /*
+4. 4 Заполненный прямоугольник
+*/
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 5) {
+            int y = 0;
+            while (y < 10) {
+                System.out.print('Q');
+                y++;
+            }
+
+            System.out.println();
+            x++;
+
+        }
+    }
+}
