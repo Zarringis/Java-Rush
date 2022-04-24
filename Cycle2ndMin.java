7bc18868361b9829367671599210807af5188965
Karina Reed <zarringis@gmail.com>
2022-04-24 10:24:20 +0300

    Изменение файла Cycle2ndMin

--- Cycle2ndMin.java
+++ Cycle2ndMin.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 */
 
 public class Cycle2ndMin {
-    //пока не знаю как вывести в минемальное число
 
     public static void main(String[] args) {
         Scanner screen = new Scanner(System.in);
