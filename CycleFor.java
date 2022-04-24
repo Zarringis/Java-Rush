1575f260970baed67cc8b9167f4b4f5ea89a33e9
Karina Reed <zarringis@gmail.com>
2022-04-24 10:19:29 +0300

    Использование цикла for

---
+++ CycleFor.java
@@ -0,0 +1,12 @@
+/*
+4. 6 Четные числа
+*/
+
+public class CycleFor {
+    public static void main(String[] args) {
+        for (int m = 1; m < 15; m++){
+            if ((m % 2) == 0)
+                System.out.println(m);
+        }
+    }
+}
