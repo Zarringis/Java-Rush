4dc30ebe958dbfb9d6b4e492651eb96d10706a48
Karina Reed <zarringis@gmail.com>
2022-04-24 10:22:09 +0300

    Использование цикла for + continue

---
+++ CycleForContinue.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+4. 6 Сумма кратных чисел
+*/
+
+public class CycleForContinue {
+    //в процессе решения
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int multiple = scanner.nextInt();
+
+        int sum = 0;
+        //напишите тут ваш код
+
+        System.out.println(sum);
+    }
+}
